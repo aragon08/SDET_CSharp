466eb604c6e743cf375c0c382104d9b61541692f
Carlos Aragon <aragon.c08@gmail.com>
2019-03-28 17:23:49 -0600

    Add solution for Exercise2

---
+++ SDET_CSharpE2/SDET_CSharpE2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SDET_CSharpE2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
